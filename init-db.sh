91e56ee808139999ee54590b7dfa77b8d66c703c
shifra-fraiman <s0548549680@gmail.com>
2024-07-09 10:50:11 +0300

    third commit

--- init-db.sh
+++ init-db.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-/opt/mssql/bin/sqlservr &
+# Start SQL Server
+"C:\Program Files\Microsoft SQL Server\MSSQL14.SQLEXPRESS\MSSQL\Binn\sqlservr.exe" &
 
 # Wait for SQL Server to start
 sleep 30
 
 # Run the SQL script to create the database
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P CopyRight#1 -d master -i /usr/src/app/your-database-script.sql
+"C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\170\Tools\Binn\SQLCMD.EXE" -S localhost -U sa -P CopyRight#1 -d master -i  ./CopyRight.sql
 
 # Keep the container running
 tail -f /dev/null
